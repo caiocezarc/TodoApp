57d5d24858ad9879272d228eee9e537ad5e52b54
Caio <ciaorazec@gmail.com>
2022-01-13 20:16:14 -0300

    Backend da aplicaçao

---
+++ backend/src/controller/TaskController.js
@@ -0,0 +1,120 @@
+const TaskModel = require('../model/TaskModel.js');
+const {
+    endOfDay, 
+    startOfDay, 
+    startOfWeek, 
+    endOfWeek,
+    startOfMonth,
+    endOfMonth,
+    startOfYear,
+    endOfYear
+} = require('date-fns');
+const currentDate = new Date();
+
+class TaskController {
+    async create(req, res) {
+        const task = new TaskModel(req.body);
+        task.save()
+            .then(success => {
+                return res.status(200).json(success);
+            })
+            .catch(error => {
+                return res.status(500).json(error);
+            })
+    }
+
+    async update(req, res) {
+        await TaskModel.findByIdAndUpdate({'_id': req.params.id}, req.body, {new: true})
+            .then(success => res.status(200).json(success))
+            .catch(error => res.status(500).json(error));
+    }
+
+    async all(req, res) {
+        await TaskModel.find({'macaddress': {'$in': req.params.macaddress}})
+            .sort('when')
+            .then(success => res.status(200).json(success))
+            .catch(error => res.status(500).json(error));
+    }
+
+    async show(req, res) {
+        await TaskModel.findById(req.params.id)
+            .then(response => {
+                if(response) {
+                    return res.status(200).json(response);
+                } else {
+                    return res.status(404).json({error: 'Tarefa não encontrada'});
+                }
+            })
+            .catch(error => {
+                return res.status(500).json(error);
+            })
+    }
+
+    async delete(req, res) {
+        await TaskModel.deleteOne({'_id': req.params.id})
+            .then(response => res.status(200).json(response))
+            .catch(error => res.status(500).json(error));
+    }
+
+    async done(req, res) {
+        await TaskModel.findByIdAndUpdate(
+            {'_id': req.params.id},
+            {'done': req.params.done},
+            {new: true})
+            .then(success => res.status(200).json(success))
+            .catch(error => res.status(500).json(error));
+        
+    }
+
+    async late(req, res) {
+        await TaskModel.find({
+            'when': {'$lt': currentDate},
+            'macaddress': {'$in': req.params.macaddress}
+        })
+        .sort('when')
+        .then(response => res.status(200).json(response))
+        .catch(error => res.status(500).json(error));
+    }
+
+    async today(req, res) {
+        await TaskModel.find({
+            'macaddress': {'$in': req.params.macaddress},
+            'when': {'$gte': startOfDay(currentDate), '$lt': endOfDay(currentDate)}
+        })
+        .sort('when')
+        .then(response => res.status(200).json(response))
+        .catch(error => res.status(500).json(error));
+    }
+
+    async week(req, res) {
+        await TaskModel.find({
+            'macaddress': {'$in': req.params.macaddress},
+            'when': {'$gte': startOfWeek(currentDate), '$lt': endOfWeek(currentDate)}
+        })
+        .sort('when')
+        .then(response => res.status(200).json(response))
+        .catch(error => res.status(500).json(error));
+    }
+
+    async month(req, res) {
+        await TaskModel.find({
+            'macaddress': {'$in': req.params.macaddress},
+            'when': {'$gte': startOfMonth(currentDate), '$lt': endOfMonth(currentDate)}
+        })
+        .sort('when')
+        .then(response => res.status(200).json(response))
+        .catch(error => res.status(500).json(error));
+    }
+
+    async year(req, res) {
+        await TaskModel.find({
+            'macaddress': {'$in': req.params.macaddress},
+            'when': {'$gte': startOfYear(currentDate), '$lt': endOfYear(currentDate)}
+        })
+        .sort('when')
+        .then(response => res.status(200).json(response))
+        .catch(error => res.status(500).json(error));
+    }
+}
+
+module.exports = new TaskController();
---
+++ backend/src/middlewares/TaskValidation.js
@@ -0,0 +1,48 @@
+const TaskModel = require('../model/TaskModel');
+const { isPast } = require('date-fns');
+
+const TaskValidation = async function(req, res, next) {
+
+    const {macaddress, type, title, description, when} = req.body;
+
+    if(!macaddress) {
+        return res.status(400).json({error: 'Macaddress é obrigatório'});
+    } else if(!type) {
+        return res.status(400).json({error: 'Tipo é obrigatório'});
+    } else if(!title) {
+        return res.status(400).json({error: 'Título é obrigatório'});
+    } else if(!description) {
+        return res.status(400).json({error: 'Descrição é obrigatória'});
+    } else if(!when) {
+        return res.status(400).json({error: 'Data e Hora são obrigatórios'});
+    } else if(isPast(new Date(when))) {
+        return res.status(400).json({error: 'Escolha uma Data e Hora futura'});
+    }
+    else {
+        let exists;
+        
+        if(req.params.id) {
+            exists = await TaskModel.findOne({
+                '_id': {'$ne': req.params.id},
+                'when': {'$eq': new Date(when)},
+                'macaddress': {'$in': macaddress}
+            });
+        } else {      
+            exists = await TaskModel.findOne({
+                'when': {'$eq': new Date(when)},
+                'macaddress': {'$in': macaddress}
+            });
+        }
+
+
+        if(exists) {
+            return res.status(400).json({error: 'Já existe uma tarefa nesse dia e hora'});
+        }
+
+        next();
+    }
+}
+
+
+
+module.exports = TaskValidation;
